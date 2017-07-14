83c4d73d36d4a3093b756b197cd43bb2fe5b43fe
asn2585 <asn2585@gmail.com>
2017-07-14 11:38:16 +0530

    lab

--- Lab.py
+++ Lab.py
@@ -1,7 +1,15 @@
-name="Amb"
-Feet=6
-inches=2
+food={"vegetables":["carrots","kale","cucumber","tomato"],"desserts":["cake","ice cream", "donut"]}
 
-print("My Name is",name "I am:"+str(feet)+str(inches)+" (feet and inches)tall"
-myvar="My Name is",name "I am:"+str(feet)+str(inches)+" (feet and inches)tall"
+cars={"sports":{"Volkswagon":"Porsche","Dodge":"Viper","Chevy":"Corvette"},"classic":{"Mercedes-Benz":"300SL","Toyota":"2000GT","Lincoln":"Continental"}}
+
+print ("the vegetables are:"("carrots","kale","cucumber","tomato")
+
+myvar(food)
+
+vegetables="carrots","kale","cucumber","tomato"
+print("the vegetables are:"vegetables)
+myvar("the vegetables are:"vegetables)
 print(myvar)
+Sports cars="Volkswagon":"Porsche","Dodge":"Viper","Chevy":"Corvette"
+print("the sports cars are:"str(Volkswagon":"Porsche","Dodge":"Viper","Chevy":"Corvette"))
+
